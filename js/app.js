3e908fb3cf3f540d0355346ad0d00c5313d5822c
Jaeyoung Choi <choi.jae.y@gmail.com>
2022-02-09 21:07:31 -0500

    fix up board write cached element

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log("hello")
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+let turn, winner
+
+
+/*------------------------ Cached Element References ------------------------*/
+const gameBoard = document.querySelector(".board")
+const circles = document.querySelectorAll(".circles")
+const gameMessages = document.querySelector("#msg")
+const startBtn = document.querySelector('#startButton')
+const resetBtn = document.querySelector('#resetButton')
+const replayBtn = document.querySelector('#replayButton')
+/*----------------------------- Event Listeners -----------------------------*/
+circles.forEach((circle) => {
+  circle.addEventListener("click", (event) => {
+    console.log(event.target)
+  })
+})
+startBtn.addEventListener("click", () => {
+  console.log("hello")
+})
+resetBtn.addEventListener("click", () => {
+  console.log("world")
+})
+replayBtn.addEventListener("click", () => {
+  console.log("!")
+})
+/*-------------------------------- Functions --------------------------------*/
+init();
+
+function init() {
+  winner = null;
+  turn = 1;
+  gameBoard = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+}
